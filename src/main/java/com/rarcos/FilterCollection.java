62df9357908a6e986520a46f69b83117cdedd1f3
Rubén Arcos <rubenarcos2@gmail.com>
2022-07-06 19:19:20 +0200

    init

--- src/main/java/com/rarcos/FilterCollection.java
+++ src/main/java/com/rarcos/FilterCollection.java
@@ -1,5 +1,3 @@
-//Filter collection so that only elements with less then 4 characters are returned.
-
 package com.rarcos;
 
 import java.util.ArrayList;
@@ -7,22 +5,79 @@ import java.util.List;
 
 public class FilterCollection {
 
-    private FilterCollection() {
-    }
+	private FilterCollection() {
+	}
 
-    public static List<String> transform7(List<String> collection) {
-        List<String> newCollection = new ArrayList<>();
-        for(String elm : collection) {
-        	if(elm.length() < 4)
-        		newCollection.add(elm);
-        }
-        return newCollection;
-    }
+	// Filter 1 - Filter collection so that only elements with less or equal then 5
+	// characters are returned.
+	public static List<String> filterShorterThant5CharactersJava7(List<String> collection) {
+		List<String> newCollection = new ArrayList<>();
+		for (String elm : collection) {
+			if (elm.length() <= 5)
+				newCollection.add(elm);
+		}
+		return newCollection;
+	}
 
-    public static List<String> transform(List<String> collection) {
-        return collection.stream()
-        		.filter(e-> e.length() < 4)
-        		.toList();
-    }
+	public static List<String> filterShorterThant5Characters(List<String> collection) {
+		return collection.stream().filter(e -> e.length() <= 5).toList();
+	}
 
+	// Filter 2 - Filter collection start with C character are returned.
+	public static List<String> filterWithCharacterCJava7(List<String> collection) {
+		List<String> newCollection = new ArrayList<>();
+		for (String elm : collection) {
+			if (elm.toUpperCase().contains("C"))
+				newCollection.add(elm);
+		}
+		return newCollection;
+	}
+
+	public static List<String> filterWithCharacterC(List<String> collection) {
+		return collection.stream().filter(e -> e.toUpperCase().contains("C")).toList();
+	}
+
+	// Filter 3 - Filter collection with distinct characters are returned.
+	public static List<String> filterWithDistinctCharactersJava7(List<String> collection) {
+		List<String> listDistinctInts = new ArrayList<>(collection.size());
+		for (String i : collection) {
+			if (!listDistinctInts.contains(i)) {
+				listDistinctInts.add(i);
+			}
+		}
+		return listDistinctInts;
+	}
+
+	public static List<String> filterWithDistinctCharacters(List<String> collection) {
+		return collection.stream().distinct().toList();
+	}
+
+	// Filter 4 - Filter collection 3 first elements are returned.
+	public static List<String> filter3FirstJava7(List<String> collection) {
+		List<String> newCollection = new ArrayList<>(collection.size());
+		newCollection.add(collection.get(0));
+		newCollection.add(collection.get(1));
+		newCollection.add(collection.get(2));
+		return newCollection;
+	}
+
+	public static List<String> filter3First(List<String> collection) {
+		return collection.stream().limit(3).toList();
+	}
+
+	// Filter 4 - Filter collection all elements minus 3 first elements are
+	// returned.
+	public static List<String> filterAllMinus3FirstJava7(List<String> collection) {
+		List<String> newCollection = new ArrayList<>(collection.size());
+
+		for (int i = 3; i < collection.size(); i++) {
+			newCollection.add(collection.get(i));
+		}
+
+		return newCollection;
+	}
+
+	public static List<String> filterAllMinus3First(List<String> collection) {
+		return collection.stream().skip(3).toList();
+	}
 }
--- src/test/java/com/rarcos/FilterCollectionSpec.java
+++ src/test/java/com/rarcos/FilterCollectionSpec.java
@@ -10,11 +10,59 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FilterCollectionSpec {
 
-    @Test
-    public void transformKeepStringsShorterThant4Characters() {
-        List<String> collection = asList("My", "name", "is", "John", "Doe");
-        List<String> expected = asList("My", "is", "Doe");
-        assertThat(transform(collection)).hasSameElementsAs(expected);
-    }
+	@Test
+	public void transformShorterThant5CharactersTest() {
+		List<String> collection = asList("Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech", "Denmark",
+				"Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia",
+				"Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
+				"Slovenia", "Spain", "Sweden");
+		List<String> expected = asList("Czech", "Italy", "Malta", "Spain");
+		assertThat(filterShorterThant5Characters(collection)).hasSameElementsAs(expected);
+	}
 
+	@Test
+	public void filterWithCharacterCTest() {
+		List<String> collection = asList("Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech", "Denmark",
+				"Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia",
+				"Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
+				"Slovenia", "Spain", "Sweden");
+		List<String> expected = asList("Croatia", "Cyprus", "Czech", "France", "Greece");
+		assertThat(filterWithCharacterC(collection)).hasSameElementsAs(expected);
+	}
+
+	@Test
+	public void filterWithDistinctsCharactersTest() {
+		List<String> collection = asList("Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech", "Denmark",
+				"Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia",
+				"Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
+				"Slovenia", "Spain", "Sweden");
+		List<String> expected = asList("Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech", "Denmark",
+				"Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia",
+				"Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
+				"Slovenia", "Spain", "Sweden");
+		assertThat(filterWithDistinctCharacters(collection)).hasSameElementsAs(expected);
+	}
+	
+	@Test
+	public void filter3FirstTest() {
+		List<String> collection = asList("Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech", "Denmark",
+				"Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia",
+				"Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
+				"Slovenia", "Spain", "Sweden");
+		List<String> expected = asList("Austria", "Belgium", "Bulgaria");
+		assertThat(filter3First(collection)).hasSameElementsAs(expected);
+	}
+	
+	@Test
+	public void filterAllMinus3FirstTest() {
+		List<String> collection = asList("Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech", "Denmark",
+				"Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia",
+				"Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
+				"Slovenia", "Spain", "Sweden");
+		List<String> expected = asList("Croatia", "Cyprus", "Czech", "Denmark",
+				"Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia",
+				"Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
+				"Slovenia", "Spain", "Sweden");
+		assertThat(filterAllMinus3First(collection)).hasSameElementsAs(expected);
+	}
 }
